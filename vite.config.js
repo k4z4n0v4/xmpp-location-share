da9ef98cf87ea8810789266b4b77993d870468a5
k4z4n0v4 <46030560+k4z4n0v4@users.noreply.github.com>
2025-12-16 23:43:11 +0400

    add ci for release and pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: process.env.VITE_BASE_PATH || '/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    assetsDir: 'assets',
+    emptyOutDir: true
   },
   server: {
     port: 3000,
